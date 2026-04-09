fd2ef9a85f78f3e7912744dad9c56ef9cb8e37c3
Matthew Keathley <mrkeathley85@gmail.com>
2026-04-09 19:57:14 -0400

    feat: add support for BroadWorks format. refactor of some parsing functionality to use shared functions. general cleanup. added BW test case when using alternative XML schema.

--- test/parser.js
+++ test/parser.js
@@ -49,8 +49,46 @@ function parseAndVerifyPayload(filename, delimiter, t) {
       t.error(err);
     });
 }
+
+function parsePayloadFromFile(filename, delimiter) {
+  return fs.readFile(`${__dirname}/data/${filename}`, 'utf8')
+    .then((data) => {
+      const segments = data.split(`\n${delimiter}`) ;
+      const regex = /.*Content-Type:\s+(.*)\n.*\n([\s\S.]*)$/;
+      const req = {payload: []} ;
+
+      for (let i = 1; i < segments.length; i++) {
+        const arr = regex.exec(segments[i]) ;
+        if (!arr) {
+          continue;
+        }
+        req.payload.push({type: arr[1], content: arr[2]}) ;
+      }
+      return parsePayload({req}) ;
+    });
+}
+
 test('parser: Broadworks SIPREC payload', (t) => {
   parseAndVerifyPayload('broadworks-offer-2.txt', '--foobar', t) ;
+}) ;
+test('parser: BroadWorks recording_metadata SIPREC payload', (t) => {
+  parsePayloadFromFile('broadworks-recording-metadata-offer.txt', '--UniqueBroadWorksBoundary')
+    .then((obj) => {
+      t.ok(obj.sdp1, 'parsed first SDP');
+      t.ok(obj.sdp2, 'parsed second SDP');
+      t.equal(obj.originalCallId, 'callhalf-95876388553:0', 'parsed BroadWorks call id');
+      t.equal(obj.caller.aor, 'sip:+15550001001@192.0.2.10', 'parsed BroadWorks caller aor');
+      t.equal(obj.caller.number, '+15550001001', 'parsed BroadWorks caller number');
+      t.equal(obj.callee.aor, 'sip:15550001002@192.0.2.10', 'parsed BroadWorks callee aor');
+      t.equal(obj.callee.number, '15550001002', 'parsed BroadWorks callee number');
+      t.equal(obj.recordingSessionId, 'urn:uuid:7ee4c89f-31d9-41e1-b6f1-8018b79b03d7', 'parsed recording session id');
+      t.end();
+      return;
+    })
+    .catch((err) => {
+      console.error(err.stack);
+      t.error(err);
+    });
 }) ;
 test('parser: Promcomm SIPREC payload', (t) => {
   parseAndVerifyPayload('procomm-siprec-offer.txt', '--2CD2A2E9', t) ;
